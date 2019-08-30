b59f3cbd9f40898e5795d211eb8c22f2a2a44330
kedrboje <fmrubchenkov@edu.hse.ru>
2019-08-30 03:14:08 +0300

    Initial Commit

---
+++ Wheather./ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wheather.
+//
+//  Created by Федор on 30/08/2019.
+//  Copyright © 2019 Федор. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
